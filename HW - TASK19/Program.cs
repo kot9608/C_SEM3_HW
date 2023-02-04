77d977661cfd3ed6d470bf856e2075496382d701
Kirill <Kot9608@mail.ru>
2023-02-04 16:08:08 +0500

    HW - TASK19_upd

--- HW - TASK19/Program.cs
+++ HW - TASK19/Program.cs
@@ -19,26 +19,3 @@ if (num1_text[0]==num1_text[4] && num1_text[1]==num1_text[3])
     return;
   }
 Console.WriteLine ($"Введеное число {num1_text} - не является полиндром");
-
-
-
-
-// Напишите программу, которая выводит третью цифру заданного числа 
-// или сообщает, что третьей цифры нет.
-
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
-
-
-// Console.Clear();
-// Console.Write("Введите число: ");
-// int num1 = int.Parse(Console.ReadLine());
-// if (num1/100==0)
-//   {
-//   Console.WriteLine ("Третьей цифры нет");
-//   return;
-//   }
-// string num1_text = Convert.ToString(num1);
-// char Result=num1_text[2];
-// Console.WriteLine (Result);
